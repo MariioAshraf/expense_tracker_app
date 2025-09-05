ac74dd0f9594aa7512d895784366decb49df8eb3
MarioAshraf <mariioashraf1@gmail.com>
2025-09-05 21:44:09 +0300

    -Added login textButton

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,6 +1,7 @@
 class Routes {
   static const loginView = '/loginView';
-  static const initialRoute = '/';
+
+  // static const initialRoute = '/';
   static const homeView = '/homeView';
   static const signUpView = '/signUpView';
 }
